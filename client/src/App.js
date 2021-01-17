3e176632782686846caa16f4b5b8f144d9329184
jesscru <jessicacrump7@gmail.com>
2021-01-16 23:56:08 -0700

    added basic file structure to client side and filled in router for App.js

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+function App() {
+  return (
+    <div >
+      <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/resume" component={Resume} />      
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
