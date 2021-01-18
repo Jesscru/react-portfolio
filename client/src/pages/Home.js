d60b173c12e5b44ebea59ba3a96cdb1f2da7e305
jesscru <jessicacrump7@gmail.com>
2021-01-18 14:19:23 -0700

    added styling to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,35 @@
 import React from "react";
+import "../styling/Home.css";
 
 function Home() {
   return (
-    <div>
-      <img src="../public/assets/images/profile-pic-bw.jpg"/>
-      <p>I’m 24, currently living in Denver, but I grew up all over the Southeast U.S.. I went to college initially pre-med and changed my mind pretty late in the game (senior year) so when I graduated I was a little lost I traveled for about six months and enjoyed the freedom of a graduate before joining the Peace Corps and committing my next two years of life to teaching English in Ecuador. In January of 2020 I moved my entire life to Ecuador. Three short months later, the pandemic plagued the world and I was evacuated back to the states. Again lost and under pressure, I was led to remember my roots—and freshman Jessica—who likes to build and create. I have a general knack for computers and languages so when faced with whether to change my career path or not, I chose what comes naturally to me. Today, I’m enrolled in UCLA Extension’s coding bootcamp. I aspire to enter the tech world using the full-stack skills I acquire with the program as well as learning through both work and self-teaching.</p>
-    </div>
+    <div class="container">
+      <div className="about-me row">  
+        <div className="col-lg-12">
+            <img src="../public/assets/images/profile-pic-bw.jpg" alt="profile of Jessica Crump" ></img>
+        </div>
+    
+        <div className="col-lg-1"></div>
+          <div className="col-lg-10">
+            <p className="about-me-text">
+                I’m currently living in Denver, but I grew up all over the Southeast U.S.. I initially started college with the 
+                intention to attend medical school afterward, but I changed my mind quite late—senior year of college, in fact. 
+                Therefore, when I graduated I was a bit lost. I traveled for about six months before joining the Peace Corps 
+                and committing the next two years of my life to teaching English in Ecuador. In January of 2020, moved my life to Ecuador. 
+                Three short months later, the pandemic plagued the world and I was evacuated back to the states. Again lost, 
+                I was led to remember my roots. I'm the type of person who loves to learn, build, and create and has a natural knack 
+                for problem solving. I've also always had a natural facility for technology—and specifically computers—so when f
+                aced with whether to change my career path or not, I chose to delve further into my natural curiosities and talents.
+                The tech world seemed a relevant next step and coding, specifically, seemed to check all the boxes. I taught myself 
+                much of the fundamentals—html, css, and javascript before enrolling in UCLA's Coding Bootcamp to learn full-stack 
+                web development. Having never coded prior, other than the month I took to teach myself those few skills, this was quite 
+                a challenge. However, I truly love pushing myself outside my comfort zone and learning new skills. Thus, I have no 
+                regrets and I cannot wait to explore this new world. </p>
+          </div>
+          <div className="col-lg-1"></div>
+        </div>
+      </div>
+  
   );
 }
 
