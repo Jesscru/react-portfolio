e9974396195fcbb5664ba39102342de18eeee117
jesscru <jessicacrump7@gmail.com>
2021-01-29 21:24:12 -0700

    updated home page blurb

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,23 +16,15 @@ function Home() {
             </div>
             <div className="col-lg-12">
                 <p className="about-me-text">
-                I’m currently living in Denver, but I grew up all over the Southeast U.S.. 
-                I initially started college with the intention to attend medical school afterward, 
-                but I changed my mind quite late—senior year of college, in fact. Therefore, when 
-                I graduated I was a bit lost. I traveled for about six months before joining the Peace 
-                Corps and committing the next two years of my life to teaching English in Ecuador. 
-                In January of 2020, I moved my life to Ecuador. Three short months later, the pandemic 
-                plagued the world and I was evacuated back to the states. Again lost, I was led to 
-                remember my roots. I'm the type of person who loves to learn, build, and create and 
-                has a natural knack for problem solving. I've also always had a natural facility for 
-                technology—and specifically computers—so when faced with whether to change my career 
-                path or not, I chose to delve further into my natural curiosities and talents. The 
-                tech world seemed a relevant next step and coding, specifically, seemed to check all
-                the boxes. I taught myself much of the fundamentals—html, css, and javascript before 
-                enrolling in UCLA's Coding Bootcamp to learn full-stack web development. Having never 
-                coded prior, other than the month I took to teach myself those few skills, this was 
-                quite a challenge. However, I truly love pushing myself outside my comfort zone and 
-                learning new skills. Thus, I have no regrets and I cannot wait to explore this new world. </p>
+                Newly certified full-stack web developer from UCLA’s coding bootcamp leveraging a diverse 
+                background of psychology, chemistry, biology, teaching, and language acquisition to use data
+                and research to build an intuitive, elegant, and comfortable user experience. As design is 
+                the first part of a project that the user experiences, I put an emphasis on making UI simple,
+                but effective with my skills in HTML, CSS, Javascript, and React.js. The projects I’ve had 
+                the pleasure of completing have truly amplified my enjoyment of teamwork dynamics—experiencing
+                the highs and lows as a unit to bring to fruition a well-functioning and refined product 
+                hat we can all be proud of. I’m excited to use the skills I’ve acquired but also to work 
+                in an environment that challenges me to grow where I’m weak as well. </p>
               </div>
             </div>
           </div>
